3c5551fde6d375843b0d064197747d3e346f885c
Vinicius-de-Morais <vmorais194@gmai.com>
2022-10-29 00:16:14 -0300

    Comment cors section, it was not letting the server load

--- Controller.js
+++ Controller.js
@@ -5,14 +5,15 @@ const bodyParser = require('body-parser');
 const { restart } = require('nodemon');
 
 const app = express();
-app.use(cors)
+//app.use(cors)
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/',(req,res)=>{
+    console.log(cors)
     res.send('Aumento peniano gratis')
 })
 
-let port = process.env.PORT || 3000;
-app.listen(port, (req,res)=>{
+let port = 3000;
+app.listen(port,(req,res)=>{
     console.log('servidor rodando');
 })
