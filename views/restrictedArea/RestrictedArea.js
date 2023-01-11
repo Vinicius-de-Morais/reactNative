17c3eebf270bc4c7094bdc518d1075f7ec3caccc
Vinicius-de-Morais <vmorais194@gmail.com>
2023-01-11 13:37:34 -0300

    add navigation tab bar

--- views/restrictedArea/RestrictedArea.js
+++ views/restrictedArea/RestrictedArea.js
@@ -1,11 +1,17 @@
 import React, {useState, useEffect, useReducer} from 'react';
 import {Text, View } from 'react-native';
+import {css} from '../../assets/css/Css';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+// import views for the bottomtab
+import {Profile, RegisterCar, EditCar} from '../index';
 
 export default function RestrictedArea(){
 
     const [user, setUser] = useState(null);
-
+    const Tab = createMaterialBottomTabNavigator();
     useEffect(()=>{
         async function getUser(){
             let response = await AsyncStorage.getItem('userData');
@@ -16,9 +22,43 @@ export default function RestrictedArea(){
     })
 
     return(
-        <View>
-            <Text>Area restrita</Text>
-            <Text>Seja bem vindo {user}</Text>
-        </View>
+
+        <Tab.Navigator
+            initialRouteName='Profile'
+            activeColor='#b2b4b7'
+            inactiveColor='#fff'
+            barStyle={css.area__tab}
+        >
+            <Tab.Screen 
+                name="Profile" 
+                component={Profile}
+                options={{
+                    tabBarLabel: 'Profile',
+                    tabBarIcon:()=>(
+                        <Icon name="users" size={20} color="#333" />
+                    )
+                }}
+            />
+            <Tab.Screen 
+                name="RegisterCar" 
+                component={RegisterCar}
+                options={{
+                    tabBarLabel: 'Register',
+                    tabBarIcon:()=>(
+                        <Icon name="archive" size={20} color="#333" />
+                    )
+                }}
+            />
+            <Tab.Screen 
+                name="EditCar" 
+                component={EditCar}
+                options={{
+                    tabBarLabel: 'Edit',
+                    tabBarIcon:()=>(
+                        <Icon name="edit" size={20} color="#333" />
+                    )
+                }}
+            />
+        </Tab.Navigator>
     )
 }
