2724c05b5d0c4380d9303f124a5777e4a026f98f
Vinicius-de-Morais <vmorais194@gmail.com>
2023-01-11 13:36:44 -0300

    create new views to tab navigation bar

---
+++ views/restrictedArea/Profile.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect, useReducer} from 'react';
+import {Text, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export default function Profile(){
+
+    return(
+        <View>
+            <Text>This is an Profile JUST BELIVE IT</Text>
+        </View>
+    )
+}
---
+++ views/restrictedArea/RegisterCar.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect, useReducer} from 'react';
+import {Text, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export default function RegisterCar(){
+
+    return(
+        <View>
+            <Text>This is an register screen JUST BELIVE IT</Text>
+        </View>
+    )
+}
