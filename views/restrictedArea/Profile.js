3f738ce1344056524c1837cf399b2ddd06ec1dd8
Vinicius-de-Morais <vmorais194@gmail.com>
2023-01-17 15:44:00 -0300

    add an button to show or hide the password change field

--- views/restrictedArea/Profile.js
+++ views/restrictedArea/Profile.js
@@ -69,7 +69,7 @@ export default function Profile({navigation}){
 
             {/* Button to change the display of the change password field */}
             <TouchableOpacity 
-                onPress={()=> setDisplayPasswordChange('flex')}
+                onPress={()=> displayPassword == 'none'? setDisplayPasswordChange('flex') : setDisplayPasswordChange('none')}
                 style={{
                     //display: 'flex',
                     backgroundColor: 'rgb(53, 53, 53)',
