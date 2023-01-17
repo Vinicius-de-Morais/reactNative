322dec1eae2bcea067515763415c5b40d34007b8
Vinicius-de-Morais <vmorais194@gmail.com>
2023-01-17 15:46:05 -0300

    add the backAction handler, to when pressed back-button we exit the app

--- views/restrictedArea/RestrictedArea.js
+++ views/restrictedArea/RestrictedArea.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {Text, View } from 'react-native';
+import {Text, View , BackHandler, Alert} from 'react-native';
 import {css} from '../../assets/css/Css';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
@@ -8,10 +8,36 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 // import views for the bottomtab
 import {Profile, RegisterCar, EditCar} from '../index';
 
-export default function RestrictedArea(){
+export default function RestrictedArea({navigation}){
 
     const Tab = createMaterialBottomTabNavigator();
     
+    // to check if the back button is pressed and exit from app
+    useEffect(() => {
+        const backAction = () => {
+            Alert.alert("Alerta!", "Deseja mesmo sair do app?", [
+                {
+                    text: "Não",
+                    onPress: () => null,
+                    style: "cancel"
+                },
+                { text: "Sim", onPress: () => {
+                    navigation.navigate('Home');
+                    BackHandler.exitApp();
+                    }
+                }
+            ]);
+            return true;
+        };
+    
+        const backHandler = BackHandler.addEventListener(
+            "hardwareBackPress",
+            backAction
+        );
+    
+        return () => backHandler.remove();
+    }, []);
+
     // here we just render the bottom tab bar to change what will bee rendered
     return(
 
